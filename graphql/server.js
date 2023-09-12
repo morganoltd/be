cdb0dbb82eae8528eaeeddf1f1a0f18a56937385
morganoltd <138611445+morganoltd@users.noreply.github.com>
2023-09-12 18:51:49 +0200

    su

--- graphql/server.js
+++ graphql/server.js
@@ -1,64 +1,63 @@
-const { ApolloServer } = require('apollo-server-express');
+const { buildSchema, GraphQLObjectType } = require('graphql');
 const express = require('express');
-const path = require('path');
-
-// Import your schema and resolvers from the graphql folder
-const GAMES = require('./graphql/GAMES/GameSchema');
-const GameMutation = require('./graphql/GAMES/GameMutation');
-const GameQuery = require('./graphql/GAMES/GameQuery');
-
-const USERS = require('./graphql/USERS/UserSchema');
-const UserMutation = require('./graphql/USERS/UserMutation');
-const UserQuery = require('./graphql/USERS/UserQuery');
-
-const POST = require('./graphql/POST/PostSchema');
-const PostMutation = require('./graphql/POST/PostMutation');
-const PostQuery = require('./graphql/POST/PostQuery');
-
-const POSTS = require('./graphql/POSTS/PostsSchema');
-const PostsMutation = require('./graphql/POSTS/PostsMutation');
-const PostsQuery = require('./graphql/POSTS/PostsQuery');
-
-const COMMENTS = require('./graphql/COMMENTS/CommentsSchema');
-const CommentsQuery = require('./graphql/COMMENTS/CommentsQuery');
-const CommentsMutation = require('./graphql/COMMENTS/CommentsMutation');
-
-const AUTH = require('./graphql/AUTH/AuthSchema');
-const AuthQuery = require('./graphql/AUTH/AuthQuery');
-const AuthMutation = require('./graphql/AUTH/AuthMutation');
-
-const AVATARS = require('./graphql/AVATARS/AvatarsSchema');
-const AvatarsQuery = require('./graphql/AVATARS/AvatarsQuery');
-const AvatarsMutation = require('./graphql/AVATARS/AvatarsMutation');
+const graphqlHTTP = require('express-graphql');
+const cors = require('cors');
+const path = require();
 
 const app = express();
 
-async function startApolloServer() {
-  const server = new ApolloServer({
-    typeDefs: [GAMES, USERS, POST, POSTS, COMMENTS, AVATARS, AUTH],
-    resolvers: [GameMutation, GameQuery, UserMutation, UserQuery, PostQuery, PostsMutation, PostsQuery, CommentsQuery, CommentsMutation, AvatarsQuery, AvatarsMutation, AuthQuery, AuthMutation],
-  });
+// Allow cross-origin
+app.use(cors());
 
-  await server.start();
+// Define your schema types and resolvers for each type
+const GAMES = require('./graphql/GAMES');
+const USERS = require('./graphql/USERS');
+const POST = require('./graphql/POST');
+const POSTS = require('./graphql/POSTS');
+const COMMENTS = require('./graphql/COMMENTS');
+const AUTH = require('./graphql/AUTH');
+const AVATARS = require('./graphql/AVATARS');
 
-  server.applyMiddleware({ app });
-
-  // Serve your static assets if needed (e.g., images, HTML files)
-  app.use(express.static(path.join(__dirname, 'public')));
-
-  // Define the route for GraphQL, adjust the path as needed
-  app.use('/graphql', (req, res) => {
-    res.status(200).send('Welcome to GraphQL!');
-  });
-
-  // Use environment variable for the port or default to 4000
-  const PORT = process.env.PORT || 4000;
-
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-}
-
-startApolloServer().catch((err) => {
-  console.error(err);
+// Combine the schemas into one
+const rootQuery = new GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    ...GAMES.query,
+    ...USERS.query,
+    ...POST.query,
+    ...POSTS.query,
+    ...COMMENTS.query,
+    ...AUTH.query,
+    ...AVATARS.query,
+  },
 });
+
+const rootMutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    ...GAMES.mutation,
+    ...USERS.mutation,
+    ...POST.mutation,
+    ...POSTS.mutation,
+    ...COMMENTS.mutation,
+    ...AUTH.mutation,
+    ...AVATARS.mutation,
+  },
+});
+
+const schema = new GraphQLSchema({
+  query: rootQuery,
+  mutation: rootMutation,
+});
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
