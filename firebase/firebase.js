5c7699b2ea9edd7b8a711c76efe9af0b9fc1e260
morganoltd <138611445+morganoltd@users.noreply.github.com>
2023-09-15 17:39:40 +0200

    k

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -3,6 +3,21 @@ require('firebase/compat/storage');
 require('firebase/compat/firestore');
 require('firebase/compat/auth');
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBoqLrKGVKefBJoFyf_D8Ayl_IQ6SBahJI",
+    authDomain: "graminator.firebaseapp.com",
+    databaseURL: "https://graminator-default-rtdb.firebaseio.com",
+    projectId: "graminator",
+    storageBucket: "graminator.appspot.com",
+    messagingSenderId: "729954657369",
+    appId: "1:729954657369:web:c63cd4e4efcbed87693d48",
+    measurementId: "G-ZFZ6EK7BHJ"
+  };
+  
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+
 const storage = firebase.storage();
 const db = firebase.firestore();
 const firebaseAuth = firebase.auth();
