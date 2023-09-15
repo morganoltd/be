a35122148d53caf91889eeb672d645a013aa1a7f
morganoltd <138611445+morganoltd@users.noreply.github.com>
2023-09-15 17:29:05 +0200

    k

--- graphql/server.js
+++ graphql/server.js
@@ -2,33 +2,12 @@ const { ApolloServer } = require('apollo-server');
 const admin = require('firebase-admin');
 const express = require('express');
 const expressGraphQL = require('express-graphql'); // Import express-graphql
-const firebase = require('firebase/compat/app');
-require('firebase/compat/storage');
-require('firebase/compat/firestore');
-require('firebase/compat/auth');
-
-const storage = firebase.storage();
-const db = firebase.firestore();
-const firebaseAuth = firebase.auth();
 
 // Initialize Firebase Admin SDK with your service account key
 const serviceAccount = require('../key.json'); // Replace with your service account key file
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBoqLrKGVKefBJoFyf_D8Ayl_IQ6SBahJI",
-  authDomain: "graminator.firebaseapp.com",
-  databaseURL: "https://graminator-default-rtdb.firebaseio.com",
-  projectId: "graminator",
-  storageBucket: "graminator.appspot.com",
-  messagingSenderId: "729954657369",
-  appId: "1:729954657369:web:c63cd4e4efcbed87693d48",
-  measurementId: "G-ZFZ6EK7BHJ"
-};
-
-firebase.initializeApp(firebaseConfig);
 
 const GAMES = require('./GAMES/GameSchema');
 const GameMutation = require('./GAMES/GameMutation');
@@ -69,5 +48,3 @@ const server = new ApolloServer({
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`Server running at ${url}`);
 });
-
-module.exports = { storage, db, firebaseAuth };
