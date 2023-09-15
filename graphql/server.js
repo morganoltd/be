c6d5d6cee1a6cec15dc704c1a648502d68d47115
morganoltd <138611445+morganoltd@users.noreply.github.com>
2023-09-15 17:23:45 +0200

    l

--- graphql/server.js
+++ graphql/server.js
@@ -17,6 +17,19 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBoqLrKGVKefBJoFyf_D8Ayl_IQ6SBahJI",
+  authDomain: "graminator.firebaseapp.com",
+  databaseURL: "https://graminator-default-rtdb.firebaseio.com",
+  projectId: "graminator",
+  storageBucket: "graminator.appspot.com",
+  messagingSenderId: "729954657369",
+  appId: "1:729954657369:web:c63cd4e4efcbed87693d48",
+  measurementId: "G-ZFZ6EK7BHJ"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 const GAMES = require('./GAMES/GameSchema');
 const GameMutation = require('./GAMES/GameMutation');
 const GameQuery = require('./GAMES/GameQuery');
